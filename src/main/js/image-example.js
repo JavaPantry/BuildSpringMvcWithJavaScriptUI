f5eea62960ab70948d34efb2943c67ab13666e0a
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-09-24 19:17:44 -0400

    Restore Json content imported from people.js module

--- src/main/js/image-example.js
+++ src/main/js/image-example.js
@@ -1,10 +1,9 @@
-//import codeURL from "./code.png"
-import codeURL from "./myCode.png"
+import codeURL from "./code.png"
+//import codeURL from "./myCode.png"
 
-const img = document.createElement("img")
-img.src = codeURL
-img.style = "background: #2B3A42;  padding: 20px"
-img.width = 32
-//document.body.appendChild(img)
+const img = document.createElement("img");
+img.src = codeURL;
+img.style = "background: #2B3A42;"; //  padding: 20px
+img.width = 32;
 const appRoot = document.getElementById("app-root");
 appRoot.appendChild(img);
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,19 +1,12 @@
-/*
-const root = document.createElement("div")
-root.innerHTML = `<p>Hello my first Webpack.</p>`
-document.body.appendChild(root)
-*/
 import { groupBy } from "lodash-es"
-//import people from "./people"
+import people from "./people"
 
 import "./style.scss"
 import './image-example'
 
-//const managerGroups = groupBy(people, "manager")
+const managerGroups = groupBy(people, "manager")
 
 const appRoot = document.getElementById("app-root");
 const root = document.createElement("div");
-// root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
-root.innerHTML = `<pre><h1>Hello Alex</h1><br/><h3>from javascript bundle.js</h3></pre>`;
-//document.body.appendChild(root)
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}  <h1>Hello Alex</h1><br/><h3>from javascript bundle.js</h3></pre>`
 appRoot.appendChild(root);
