be7de64d834173514e0181f781412255fecb8b05
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-09-25 11:20:24 -0400

    introduce Axios - add Axios dependency - add REST response to '/react/data' on java side - render response in index.js

--- src/main/java/my/groupid/home/ReactController.java
+++ src/main/java/my/groupid/home/ReactController.java
@@ -3,6 +3,10 @@ package my.groupid.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 class ReactController {
@@ -15,5 +19,51 @@ class ReactController {
 	@GetMapping("/react")
 	String about() {
 		return "home/react";
+	}
+
+	@GetMapping("/react/data")
+	@ResponseBody
+	List<A> getDummyData(){
+		List<A> result = new ArrayList<A>();
+		result.add(new A(0l,"Alex", "programmer"));
+		result.add(new A(2l,"Bohdan", "manipulator"));
+		result.add(new A(3l,"Hong", "programmer"));
+		return result;
+	}
+}
+
+class A{
+	Long	id;
+	String	name;
+	String	description;
+
+	public A(Long id, String name, String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 }
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,12 +1,25 @@
 import { groupBy } from "lodash-es"
 import people from "./people"
+import { API } from './utils/api';
 
 import "./style.scss"
 import './image-example'
 
-const managerGroups = groupBy(people, "manager")
+const managerGroups = groupBy(people, "manager");
+
+
 
 const appRoot = document.getElementById("app-root");
 const root = document.createElement("div");
-root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}  <h1>Hello Alex</h1><br/><h3>from javascript bundle.js</h3></pre>`
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}  <h1>Hello Alex</h1><br/><h3>from javascript bundle.js</h3></pre>`;
 appRoot.appendChild(root);
+
+API.get('/react/data')
+    .then(response => {
+        let data = response.data;
+        const root2 = document.createElement("div");
+        root2.innerHTML = `<br/><pre>${JSON.stringify(data, null, 2)}</pre>`;
+        appRoot.appendChild(root2);
+    })
+    .catch(error => console.log(error));
+
