20435c7129f3a6b6f469ef4f1d1be7424c1a586f
Alexei Ptitchkin <ptitchkin@gmail.com>
2018-09-24 21:28:16 -0400

    add TODO output path for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ module.exports = {
   //entry: './src/index.js',
   entry: './src/main/js/index.js',
   output: {
-    filename: 'bundle.js',
-    //path: path.resolve(__dirname, 'dist')
-	//path: path.resolve(__dirname, './src/main/webapp/WEB-INF/views/home')
-	//path: path.resolve(__dirname, './src/main/webapp/resources/js')
-    path: path.resolve(__dirname, './src/main/webapp')
-    //need to read about  publicPath: ""
+		filename: 'bundle.js',
+		//path: path.resolve(__dirname, 'dist')
+		//path: path.resolve(__dirname, './src/main/webapp/WEB-INF/views/home')
+		//path: path.resolve(__dirname, './src/main/webapp/resources/js')
+		path: path.resolve(__dirname, './src/main/webapp')
+		// TODO: path: path.resolve(__dirname, 'target/scaffolding-mvc-version'),
 
-  },
+	},
    module: {
      rules: [
        {
